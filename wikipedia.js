fdfc3cfa3304b5de3ef0bfb8b5c49842e91d71d8
theallisonbrett <theallisonbrett@gmail.com>
2017-05-30 19:34:20 -0700

    Fix bug by overriding form submit's default function. https://stackoverflow.com/questions/44274533/why-wont-my-wikipedia-opensearch-ajax-request-return-successfully

--- wikipedia.js
+++ wikipedia.js
@@ -1,25 +1,22 @@
 $(document).ready(function() {
+  $("form").submit(function(e) {
+    e.preventDefault();
+    var searchTerm = $("#searchterm").val();
+    var proxy = 'https://cors-anywhere.herokuapp.com/';
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
+      searchTerm + "&format=json";
 
-$("form").submit(function() {
-  var searchTerm = $("#searchterm").val();
-  var proxy = 'https://cors-anywhere.herokuapp.com/';
-  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
-  searchTerm + "&format=json";
+    $.ajax({
+      url: proxy + url,
 
-  $.ajax( {
-    url: proxy + url,
+      success: function(response) {
+        console.log(response);
+      },
 
-    success: function(response) {
-       console.log(response);
-    },
-
-    error: function (request, status, error) {
-       console.log(request.responseText);
-       console.log(url);
-    }
-
-});
-});
-
-
+      error: function(request, status, error) {
+        console.log(request.responseText);
+        console.log(url);
+      }
+    });
   });
+});
